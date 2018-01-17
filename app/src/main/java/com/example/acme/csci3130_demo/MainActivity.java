1c11b827fe874918fa929a75332bdc1209aede7d
Haohai Deng <hh389677@dal.ca>
2018-01-17 16:53:06 -0400

    Added a new function for text changing
    The user should input text on the field and click the button after. The text from the field should then be copied to the textbox that was already in the layout.

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText text;
+    TextView output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button submit = (Button) findViewById(R.id.button);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text = (EditText)findViewById(R.id.editText2);
+                output = (TextView)findViewById(R.id.textView);
+                output.setText(text.getText());
+            }
+        });
     }
 }
